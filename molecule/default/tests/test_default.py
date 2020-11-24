997599833cf7af4a5a4d9000fe1aa316cac0a318
darkwizard242 <am900820@gmail.com>
2020-11-24 11:01:59 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,49 +6,86 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE = 'docker-ce'
+PACKAGE_SUPPORT1 = 'docker-ce-cli'
+PACKAGE_SUPPORT2 = 'containerd.io'
+PACKAGE_USER = 'docker'
+PACKAGE_GROUP = 'docker'
+PACKAGE_HOME = '/home/docker'
+PACKAGE_BINARY = "/usr/bin/docker"
+
+
 def test_docker_user_exists(host):
-    assert host.user('docker')
+    """
+    Tests if docker user exists.
+    """
+    assert host.user(PACKAGE_USER)
 
 
 def test_docker_group_exists(host):
-    assert host.group('docker')
+    """
+    Tests if docker group exists.
+    """
+    assert host.group(PACKAGE_GROUP)
 
 
 def test_docker_home_exists(host):
-    assert host.file('/home/docker').exists
+    """
+    Tests if /home/docker directory exists.
+    """
+    assert host.file(PACKAGE_HOME).exists
 
 
 def test_docker_cli_package_installed(host):
-    assert host.package("docker-ce-cli").is_installed
+    """
+    Tests if docker-ce-cli package is in installed state.
+    """
+    assert host.package(PACKAGE_SUPPORT1).is_installed
 
 
 def test_docker_containered_package_installed(host):
-    assert host.package("containerd.io").is_installed
+    """
+    Tests if containerd.io package is in installed state.
+    """
+    assert host.package(PACKAGE_SUPPORT2).is_installed
 
 
 def test_docker_package_installed(host):
-    assert host.package("docker-ce").is_installed
+    """
+    Tests if docker-ce package is in installed state.
+    """
+    assert host.package(PACKAGE).is_installed
 
 
 def test_docker_binary_exists(host):
-    assert host.file('/usr/bin/docker').exists
+    """
+    Tests if docker binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_docker_binary_file(host):
-    assert host.file('/usr/bin/docker').is_file
+    """
+    Tests if docker binary is a file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_docker_repo_exists(host):
+    """
+    Tests if relevant repository file exists for docker.
+    """
     assert host.file('/etc/apt/sources.list.d/docker-ce.list').exists or \
-      host.file('/etc/yum.repos.d/docker-ce.repo').exists or \
-      host.file('/etc/apt/sources.list.d/docker-ce.list').exists
+        host.file('/etc/yum.repos.d/docker-ce.repo').exists
 
 
 def test_docker_repo_file(host):
+    """
+    Tests if relevant repository file exists for docker.
+    """
     assert host.file('/etc/apt/sources.list.d/docker-ce.list').is_file or \
-      host.file('/etc/yum.repos.d/docker-ce.repo').is_file or \
-      host.file('/etc/apt/sources.list.d/docker-ce.list').is_file
+        host.file('/etc/yum.repos.d/docker-ce.repo').is_file
 
 
 def test_docker_binary_which(host):
-    assert host.check_output('which docker') == '/usr/bin/docker'
+    assert host.check_output('which docker') == PACKAGE_BINARY
